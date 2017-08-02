8b6310be56dc5d4e1fb96c8b2fdb3a6e91d6d98a
Vega <mativega@amazon.com>
2017-08-02 02:43:25 +0200

    N-1. Polindrom

---
+++ Java/palindrom.java
@@ -0,0 +1,42 @@
+import java.io.*;  
+  
+public class PalindromString  
+{  
+	/**
+	* Write a program which takes input as string and check it is palindrom or not.
+	* If the string and the reverse of string are same then we say string is palindrom. 
+	* Example : 1.If string is "level" then the output should be: palindrome 2.
+	* If string is "helloworld" then the output should be: not a palindrome
+	*/
+	public static void main(String a[]) throws Exception  
+	{  
+           String strn;  
+           int flag=0;  
+           System.out.println("Enter the string:");  
+           BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+           strn=br.readLine();  
+           System.out.println("Result string is:");  
+           //write your logic here  
+           char[] strnArray = strn.toCharArray();
+           String newStrn = "";
+           
+           for(char letra : strn.toCharArray()){
+               newStrn = letra + newStrn;
+           }
+            
+           if(newStrn.equals(strn)){
+               flag = 1;
+           }
+  
+  
+            System.out.println(newStrn);
+  
+           //end  
+           if(flag==1)  
+               System.out.print("palindrome");  
+           else   
+               System.out.print("not a palindrome");  
+  
+        }  
+}  
+
